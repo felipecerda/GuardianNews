f47b19ab7905a1d4bf748c7673c78b58a88f0b15
Felipe Cerda <felipecerdabarra@gmail.com>
2020-05-21 22:50:41 +0200

    chore: Added NewsAdapter

--- app/src/main/java/com/example/guardiannews/NewsActivity.java
+++ app/src/main/java/com/example/guardiannews/NewsActivity.java
@@ -1,14 +1,96 @@
 package com.example.guardiannews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class NewsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<List<NewsItem>> {
+
+    private static final String REQUEST_URL = "https://content.guardianapis.com/search?";
+    public static final String LOG_TAG = NewsActivity.class.getName();
+    private static final int NEWS_LOADER_ID = 1;
+    private NewsAdapter mAdapter;
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView newsListView = findViewById(R.id.list);
+
+        mAdapter = new NewsAdapter(this, new ArrayList<NewsItem>());
+        newsListView.setAdapter(mAdapter);
+
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current news item that was clicked on
+                NewsItem currentNewsItem = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri newsItemUri = Uri.parse(currentNewsItem.getUrl());
+
+                // Create a new intent to view the news item URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsItemUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            View loadingIndicator = findViewById(R.id.loading_spinner);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
+    }
+
+    @Override
+    public Loader<List<NewsItem>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<NewsItem>> loader, List<NewsItem> newsItems) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<NewsItem>> loader) {
+
     }
 }
---
+++ app/src/main/java/com/example/guardiannews/NewsAdapter.java
@@ -0,0 +1,65 @@
+package com.example.guardiannews;
+
+import android.app.Activity;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class NewsAdapter extends ArrayAdapter<NewsItem> {
+
+    public NewsAdapter(Activity context, ArrayList<NewsItem> newsItems){
+        super(context, 0, newsItems);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_list_item, parent, false);
+        }
+
+        NewsItem currentNewsItem = getItem(position);
+
+        String title = currentNewsItem.getTitle();
+        String section = currentNewsItem.getSection();
+        String author = currentNewsItem.getAuthor();
+
+        TextView titleView = listItemView.findViewById(R.id.title);
+        titleView.setText(title);
+
+        TextView sectionView = listItemView.findViewById(R.id.section);
+        sectionView.setText(section);
+
+        TextView authorView = listItemView.findViewById(R.id.author);
+        authorView.setText(author);
+
+        //Formats the date
+        DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' hh:mm a");
+        LocalDateTime localDateTime = currentNewsItem.getDate();
+        String formattedDate = FORMATTER.format(localDateTime);
+
+        TextView dateView = listItemView.findViewById(R.id.date_time);
+        dateView.setText(formattedDate);
+
+        return listItemView;
+    }
+
+
+
+}
